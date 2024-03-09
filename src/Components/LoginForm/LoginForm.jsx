9cccfc3244e0b9122125b108a4bc5ce0e440dfe3
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2024-03-08 21:24:32 -0500

    Función Asincrona Login

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,42 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdEmail } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
 import "./LoginForm.css";
+import { useNavigate } from "react-router-dom";
+
+// import axios from "axios";
+// import qs from "qs";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function login() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("email", email);
+    urlencoded.append("current_password", password);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    fetch("http://192.168.1.7:5000/api/v1/auth/login", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  }
+
   return (
     <div className="wrapper">
-      <form action="">
-        <div className="header">
-          <img src="src\Components\assets\unimentor_icon.png" alt="" />
-          <div className="header_titles">
-            <h1>UniMentor</h1>
-            <p>Llevando la Claridad a Cada Sesión de Estudio</p>
-          </div>
+      <div className="header">
+        <img src="src\Components\assets\unimentor_icon.png" alt="" />
+        <div className="header_titles">
+          <h1>UniMentor</h1>
+          <p>Llevando la Claridad a Cada Sesión de Estudio</p>
         </div>
+      </div>
 
-        <div className="login">
-          <h2>INICIA SESION</h2>
-        </div>
+      <div className="login">
+        <h2>INICIA SESION</h2>
+      </div>
 
-        <div className="input-box">
-          <input type="text" placeholder="Correo Institucional" />
-          <MdEmail className="icon" />
-        </div>
+      <div className="input-box">
+        <input
+          type="text"
+          placeholder="Correo Institucional"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <MdEmail className="icon" />
+      </div>
 
-        <div className="input-box">
-          <input type="password" placeholder="Contraseña" />
-          <FaLock className="icon" />
-        </div>
+      <div className="input-box">
+        <input
+          type="password"
+          placeholder="Contraseña"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <FaLock className="icon" />
+      </div>
 
-        <div className="remember">
-          <label>
-            <input type="checkbox" /> Recuerdame
-          </label>
-        </div>
+      <div className="remember">
+        <label>
+          <input type="checkbox" /> Recuerdame
+        </label>
+      </div>
 
-        <button type="submit">Ingresar</button>
-      </form>
+      <button onClick={login}>Ingresar</button>
     </div>
   );
 };
---
+++ src/pages/MonitorHome/MonitorHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MonitorHome.css'
+
+
+const MonitorHome = () => {
+  return (
+    <div>
+      <h1 className='titulo'>Monitor Home</h1>
+    </div>
+  )
+}
+
+export default MonitorHome
